d881146dad3d8a134a8d38747aba7534258bc337
august0cesar <augustocesarmatos@hotmail.com>
2020-08-28 16:54:36 -0300

    Implemenatndo codigo do jogo de forca

--- game_forca.py
+++ game_forca.py
@@ -3,6 +3,22 @@ def jogar():
     print("### Bem Vindo ao  Jogo de Forca ########")
     print("########################################")
 
+    palavra_chave = "banana"
+    acertou       = False
+    inforcado     = False
+
+    lista_palavras_certas = ["_","_","_","_","_","_"]
+    print(lista_palavras_certas)
+    while(not acertou and not inforcado):
+        chute         = input("Digite uma letra ")
+        index         = 0
+        for letra in palavra_chave:
+            if(chute.upper() == letra.upper()):
+                # print("Letra {} encontrada na posicao {}".format(letra,index))
+                lista_palavras_certas[index] = letra
+            index += 1
+            # print(letra)
+        print(lista_palavras_certas)    
 
     print("Jogo Finalizado")
 
--- start_game.py
+++ start_game.py
@@ -1,6 +1,6 @@
 import game_adivinhacao
 import game_forca
-
+from be import be
 def iniciar():
 
     print("########################################")
@@ -8,7 +8,7 @@ def iniciar():
     print("########################################")
 
 
-    print("(1) Forca (2) Adivinhacao")
+    print("(1) Forca (2) Adivinhacao (3) test")
 
     game = int(input("Qual jogo? "))
     if(game == 1):
@@ -17,6 +17,12 @@ def iniciar():
     elif(game == 2):
         print("Abrindo jogo Adivinhacao")
         game_adivinhacao.jogar()
+    elif(game == 3):
+        be.test()
+
+
+
+
 
 if(__name__ =="__main__"):
     iniciar()
