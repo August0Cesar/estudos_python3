254a16fd764d40db9be77704c82d5ca12adf4c77
august0cesar <augustocesarmatos@hotmail.com>
2020-08-28 16:56:54 -0300

    Removendo diretorios e arquios desnecessarios

--- .history/game_forca_20200828144623.py
+++
@@ -1,7 +0,0 @@
-def jogo:
-    print("########################################")
-    print("### Bem Vindo ao  Jogo de Forca ########")
-    print("########################################")
-
-
-    print("Jogo Finalizado")
